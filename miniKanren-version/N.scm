ef52d4900f0d359918f96d9d9fe668cd8bc25c00
William Byrd <webyrd@gmail.com>
2020-08-30 06:11:04 -0400

    working on miniKanren version

--- miniKanren-version/N.scm
+++ miniKanren-version/N.scm
@@ -3,6 +3,8 @@
 (define lookupo
   (lambda (x env val)
     (fresh (y v env^)
+      (symbolo x)
+      (symbolo y)
       (== `((,y . ,v) . ,env^) env)
       (conde
         ((== x y) (== v val))
@@ -14,6 +16,7 @@
     (conde
       ((fresh (x)
          (== `(Var ,x) expr)
+         (symbolo x)
          (lookupo x env val)))
       ((fresh (f x vf vx)
          (== `(App ,f ,x) expr)
@@ -22,6 +25,7 @@
          (apply-expro vf vx val)))
       ((fresh (x b)
          (== `(Lam ,x ,b) expr)
+         (symbolo x)
          (== `(Closure ,env ,x ,b) val))))))
 
 (define apply-expro
@@ -29,19 +33,42 @@
     (conde
       ((fresh (env x b)
          (== `(Closure ,env ,x ,b) f)
+         (symbolo x)
          (eval-expro `((,x . ,v) . ,env) b val)))
       ((fresh (n)
          (== `(N ,n) f)
          (== `(N (NApp ,n ,v)) val))))))
 
-#|
-(define fresh
-  (lambda (xs x)
-    (cond
-      ((member x xs)
-       (fresh xs (string->symbol (string-append (symbol->string x) "^"))))
-      (else x))))
+(define fresho
+  (lambda (xs x x^)
+    (fresh ()
+      (symbolo x)
+      (symbolo x^)
+      (conde
+        ((membero x xs)
+         (fresh (x^^)
+           (fresho xs x^^ x^)))
+        ((== x x^)
+         (not-membero x xs))))))
 
+(define membero
+  (lambda (x ls)
+    (fresh (y ls^)
+      (== `((,y . ,ls^)) ls)
+      (conde
+        ((== x y))
+        ((=/= x y) (membero x ls^))))))
+
+(define not-membero
+  (lambda (x ls)
+    (conde
+      ((== '() ls))
+      ((fresh (y ls^)
+         (== `((,y . ,ls^)) ls)
+         (=/= x y)
+         (not-membero x ls^))))))
+
+#|
 (define uneval-value
   (lambda (xs v)
     (pmatch v
