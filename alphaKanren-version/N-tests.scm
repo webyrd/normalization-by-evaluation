8518bfebecbe04a8f94fbfceac5a277e955a0b70
William Byrd <webyrd@gmail.com>
2020-08-30 14:01:02 -0400

    added comment about alphKanren behavior

--- alphaKanren-version/N-tests.scm
+++ alphaKanren-version/N-tests.scm
@@ -1,15 +1,51 @@
 (load "N.scm")
 (load "../scheme-helpers/test-macro.scm")
 
-;; WEB (fresh (a) (exist (x) (== a x) (hash x a))) signals an error,
-;; since the first arg to `hash` is a nom, even though (fresh (a)
-;; (exist (x) (== a x) (hash a a))) is legal.
+;; WEB comment:
 ;;
-;; This feels overly restrictive.  Once we unify `x` and `a`, shouldn't
-;; uses of `x` behave like uses of `a`?  Of course,
-;; (fresh (a) (exist (x) (hash x a) (== a x))) should work the same as
-;; (fresh (a) (exist (x) (== a x) (hash x a))).  How does alphaProlog
-;; behave in this case?
+;; The current implementation of alphaKanren uses *restricted nominal unification*,
+;; as described on p. 35 of "Nominal Logic Programming" by Cheney and Urban
+;; (http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.342.6780)
+;;
+;; As a result, the first argument to a `hash` constraint *must* be a "nom" ("atom").
+;; The first argument to a `hash` constraint *must not* be a "normal" unification
+;; variable, even if that unification variable has been unified with a nom.
+;;
+;; For example,
+;;
+;; (fresh (a) (exist (x) (== a x) (hash x a)))
+;;
+;; is illegal and signals an error, since the first argument to `hash` is not a nom, even though
+;;
+;; (fresh (a) (exist (x) (== a x) (hash a a)))
+;;
+;; is legal.
+;;
+;; In other words, unifying the unification variable `x` with the nom `a`, does *not* allow us
+;; to treat `x` as if it were `a`.
+;;
+;; This is a super annoying restriction, and feels very non-miniKanreny.
+;;
+;; As a consolation, at least it is the case that
+;;
+;; (fresh (a) (exist (x) (hash x a) (== a x)))
+;;
+;; and
+;;
+;; (fresh (a) (exist (x) (== a x) (hash x a)))
+;;
+;; have identical behavior--both signal errors.
+;;
+;; Has there been any more progress in the nominal unification
+;; literature in lifting this restriction?
+;;
+;; Perhaps we could modify the alphaKanren implementation to allow the
+;; constraints requiring the first argument to be a nom to be
+;; *delayed*.  These delayed goals would suspend their evaluation
+;; until their first argument became ground to a nom.  An error would
+;; be signalled if the `run` expression finished execution without all
+;; of the delayed goals being evaluated.
+
 
 (test "simple-nominal-1"
   (run* (q)
