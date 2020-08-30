8ad0a623d913bf1901cb568a8b8298d87e0370f4
William Byrd <webyrd@gmail.com>
2020-08-30 10:43:27 -0400

    adding nominal tests

--- alphaKanren-version/N-tests.scm
+++ alphaKanren-version/N-tests.scm
@@ -1,3 +1,34 @@
 (load "N.scm")
 (load "../scheme-helpers/test-macro.scm")
 
+(test "lookupo-1"
+  (run* (q)
+    (fresh (a b c)
+      (exist (env)
+        (== `((,a . 1) (,b . 2) (,c . 3)) env)
+        (lookupo b env q))))
+  '(2))
+
+(test "lookupo-2"
+  (run* (q)
+    (fresh (a b c)
+      (exist (env)
+        (== `((,a . 1) (,b . 2) (,c . 3) (,b . ,4)) env)
+        (lookupo b env q))))
+  '(2))
+
+(test "lookupo-3"
+  (run* (q)
+    (fresh (a b c d)
+      (exist (env)
+        (== `((,a . 1) (,b . 2) (,c . 3)) env)
+        (lookupo d env q))))
+  '())
+
+(test "lookupo-4"
+  (run 3 (env)
+    (fresh (z)
+      (lookupo z env 1)))
+  '(((a.0 . 1) . _.0)
+    (((_.0 . _.1) (a.0 . 1) . _.2) : ((a.0 . _.0)))
+    (((_.0 . _.1) (_.2 . _.3) (a.0 . 1) . _.4) : ((a.0 . _.0) (a.0 . _.2)))))
