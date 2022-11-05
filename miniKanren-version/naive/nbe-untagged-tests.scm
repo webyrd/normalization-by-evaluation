c04809e1cc0788db139077635a14a7bd22edde0b
William Byrd <webyrd@gmail.com>
2022-11-04 23:54:11 -0400

    working on untagged naive version

--- miniKanren-version/naive/nbe-untagged-tests.scm
+++ miniKanren-version/naive/nbe-untagged-tests.scm
@@ -7,12 +7,12 @@
       (eval-expro '(lambda (x) x) '() id_)
       (eval-expro '(lambda (x) (lambda (y) x)) '() const_)
       (eval-expro '(App const id) `((id . ,id_) (const . ,const_)) result)))
-  '((closure y x ((x . (closure x x ()))))))
+  '((closure (y) x ((x . (closure (x) x ()))))))
 
 (test "eval-expro-1"
   (run* (val)
     (eval-expro `(lambda (z) z) '() val))
-  '((closure z z ())))
+  '((closure (z) z ())))
 
 (test "eval-expro-2"
   (run* (val)
@@ -21,7 +21,7 @@
            (lambda (z) z))
      '()
      val))
-  '((closure y x ((x . (closure z z ()))))))
+  '((closure (y) x ((x . (closure (z) z ()))))))
 
 (test "uneval-valueo-0"
   (run 6 (val expr)
@@ -32,24 +32,24 @@
     (((N (NApp (NVar _.0) (N (NVar _.1))))
       (App _.0 _.1))
      (sym _.0 _.1))
-    (((closure _.0 _.0 _.1)
+    (((closure (_.0) _.0 _.1)
       (lambda (_.2) _.2))
      (sym _.0 _.2))
     (((N (NApp (NApp (NVar _.0) (N (NVar _.1))) (N (NVar _.2))))
       (App (App _.0 _.1) _.2))
      (sym _.0 _.1 _.2))
-    (((closure _.0 _.1 ((_.1 N (NVar _.2)) . _.3))
+    (((closure (_.0) _.1 ((_.1 N (NVar _.2)) . _.3))
       (lambda (_.4) _.2))
      (=/= ((_.0 _.1)))
      (sym _.0 _.1 _.2 _.4))
-    (((closure _.0 (lambda (_.1) _.1) _.2)
+    (((closure (_.0) (lambda (_.1) _.1) _.2)
       (lambda (_.3) (lambda (_.4) _.4)))
      (=/= ((_.3 _.4)))
      (sym _.0 _.1 _.3 _.4))))
 
 (test "uneval-valueo-1"
   (run* (expr)
-    (uneval-valueo '() '(closure y x ((x . (closure z z ())))) expr))
+    (uneval-valueo '() '(closure (y) x ((x . (closure (z) z ())))) expr))
   '(((lambda (_.0) (lambda (_.1) _.1))
      (=/= ((_.0 _.1)))
      (sym _.0 _.1))))
