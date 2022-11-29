706e9d2e0adbf74e5d33e27880ae5349f60654c0
William Byrd <webyrd@gmail.com>
2022-11-29 04:49:34 -0500

    added test

--- miniKanren-version/naive/nbe-untagged-full-tests.scm
+++ miniKanren-version/naive/nbe-untagged-full-tests.scm
@@ -1,3 +1,8 @@
 (load "nbe-untagged-full.scm")
 (load "../faster-miniKanren/test-check.scm")
 
+(test "foo-1"
+  (run* (expr)
+    (nfo '(lambda (x) ((lambda (y) y) 5))
+         expr))
+  '(((lambda (_.0) 5) (sym _.0))))
