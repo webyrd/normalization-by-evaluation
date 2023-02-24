8e9f7eb095df38c328d08ad9bfe6c085ef587281
William Byrd <webyrd@gmail.com>
2023-02-23 19:33:00 -0500

    updated formatting

--- miniKanren-version/naive/rbe3.scm
+++ miniKanren-version/naive/rbe3.scm
@@ -25,17 +25,17 @@
   (lambda (expr env val)
     (conde
       ((fresh (x body)
-              (== `(lambda (,x) ,body) expr)
-              (== `(closure (,x) ,body ,env) val)
-              (symbolo x)))
+         (== `(lambda (,x) ,body) expr)
+         (== `(closure (,x) ,body ,env) val)
+         (symbolo x)))
       ((symbolo expr) (lookupo expr env val))
       ((fresh (e1 e2 f v x body env^)
-              (== `(,e1 ,e2) expr)
-              (== `(closure (,x) ,body ,env^) f)
-              (symbolo x)
-              (eval-expro e1 env f)
-              (eval-expro e2 env v)
-              (eval-expro body `((,x . ,v) . ,env^) val))))))
+         (== `(,e1 ,e2) expr)
+         (== `(closure (,x) ,body ,env^) f)
+         (symbolo x)
+         (eval-expro e1 env f)
+         (eval-expro e2 env v)
+         (eval-expro body `((,x . ,v) . ,env^) val))))))
 
 
 ;; Fast and simple fresho definition (written with Michael Ballantyne)
@@ -49,7 +49,7 @@
 
 (define (reduceo xs expr env expr^)
   (conde
-    ; lambda stays a lambda
+    ;; lambda stays a lambda
     [(fresh (x body x^ body^)
        (== `(lambda (,x) ,body) expr)
        (== `(lambda (,x^) ,body^) expr^)
@@ -57,15 +57,15 @@
        (symbolo x^)
        (fresho xs x^)
        (reduceo `(,x^ . ,xs)
-                 body
-                 `((,x . (NVar ,x^)) . ,env)
-                 body^))]
-    ; var stays a var
+                body
+                `((,x . (NVar ,x^)) . ,env)
+                body^))]
+    ;; var stays a var
     [(symbolo expr)
      (symbolo expr^)
      (lookupo expr env `(NVar ,expr^))]
 
-    ; var looks up to a closure
+    ;; var looks up to a closure
     [(fresh (x body x^ body^ env^)
        (symbolo expr)
        (== `(lambda (,x^) ,body^) expr^)
@@ -73,30 +73,30 @@
        (fresho xs x^)
        (lookupo expr env `(closure (,x) ,body ,env^))
        (reduceo `(,x^ . ,xs)
-                 body
-                 `((,x . (NVar ,x^)) . ,env)
-                 body^)
-       )]
+                body
+                `((,x . (NVar ,x^)) . ,env)
+                body^))]
 
-    ; app stays an app
+    ;; app stays an app
     [(fresh (e1 e2 e1^ e2^)
        (== `(,e1 ,e2) expr)
        (== `(,e1^ ,e2^) expr^)
        (reduceo xs e1 env e1^)
        (reduceo xs e2 env e2^))]
 
-    ; app reduces
+    ;; app reduces
 
-    ; It makes sense that to reduce an appplication, we have to obtain a value for the rator.
-    ; But, we also force the operand to reduce. I think this means that this version is implementing Beta-value
-    ; reduction, not general beta.
+    ;; It makes sense that to reduce an appplication, we have to
+    ;; obtain a value for the rator.  But, we also force the operand
+    ;; to reduce. I think this means that this version is implementing
+    ;; Beta-value reduction, not general beta.
     [(fresh (e1 e2 f v x body env^)
-        (== `(,e1 ,e2) expr)
-        (== `(closure (,x) ,body ,env^) f)
-        (symbolo x)
-        (eval-expro e1 env f)
-        (eval-expro e2 env v)
-        (reduceo xs body `((,x . ,v) . ,env^) expr^))]))
+       (== `(,e1 ,e2) expr)
+       (== `(closure (,x) ,body ,env^) f)
+       (symbolo x)
+       (eval-expro e1 env f)
+       (eval-expro e2 env v)
+       (reduceo xs body `((,x . ,v) . ,env^) expr^))]))
 
 (define rfo
   (lambda (e e^)
@@ -133,4 +133,3 @@
      (=/= ((_.0 _.1)) ((_.0 f)) ((_.1 f)) ((_.2 _.3)) ((_.2 f))
           ((_.3 f)))
      (sym _.0 _.1 _.2 _.3))))
-
